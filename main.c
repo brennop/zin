0d6778ab228731f09a1d4cf5e237a0f5c482be7d
brennop <brennopcordeiro@gmail.com>
2022-03-29 13:16:07 -0300

    working file server

--- main.c
+++ main.c
@@ -6,15 +6,19 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define PORT 8000
+#define PORT 8080
 #define MAXLINE 1024
 #define MAX_CLIENTS 10
 
 void *handle_connection(void *);
+char *ROOT;
 
 int main() {
   int listen_fd, connection_fd;
   struct sockaddr_in addr;
+
+  /** caminho para servir os arquivos */
+  ROOT = getenv("PWD");
 
   /** cria um socket */
   listen_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -43,32 +47,69 @@ int main() {
 }
 
 void *handle_connection(void *arg) {
+  /** descritor da conexão */
   int connection_fd = *(int *)arg;
-  char buffer[MAXLINE];
 
+  /** buffer para receber dados */
+  char buffer[MAXLINE];
+  memset(buffer, 0, MAXLINE);
+
+  char path[MAXLINE];
+
+  /** le os dados recebidos */
   int received = read(connection_fd, buffer, MAXLINE);
 
   if (received < 0) {
-    fprintf(stderr, "Erro ao ler do socket\n");
+    fprintf(stderr, "Erro ao ler do socket.\n");
   } else if (received == 0) {
-    fprintf(stderr, "Conexao fechada\n");
+    fprintf(stderr, "Conexao fechada.\n");
   } else {
-    printf("%s\n", buffer);
+    printf("%s\n", buffer); /** imprime o que foi recebido */
+
+    /** faz um "parsing" do que foi recebido */
+    char *method = strtok(buffer, " \t\r\n"); // metodo http
+    char *uri = strtok(NULL, " \t");          // caminho do arquivo
+    char *prot = strtok(NULL, " \t\r\n");     // versão do protocolo
+
+    if (strncmp(method, "GET", 4) == 0) {
+      /** se nenhum arquivo foi especificado, usa o index.html */
+      if (strncmp(uri, "/", 2) == 0) {
+        uri = "/index.html";
+      }
+
+      strcpy(path, ROOT);
+      strcpy(&path[strlen(ROOT)], uri);
+      printf("file: %s\n", path);
+
+      /** tenta abrir o arquivo */
+      FILE *file = fopen(path, "rb");
+
+      if (file == NULL) {
+        printf("404: Arquivo nao encontrado.\n");
+        /** se o arquivo não existir, retorna um erro 404 */
+        char *error_msg = "HTTP/1.1 404 Not Found\r\n\r\n";
+        write(connection_fd, error_msg, strlen(error_msg));
+      } else {
+        /** se o arquivo existir, retorna o conteúdo do arquivo */
+        char *ok_msg = "HTTP/1.1 200 OK\r\n\r\n";
+        write(connection_fd, ok_msg, strlen(ok_msg));
+
+        /** limpa o buffer antes de enviar o arquivo */
+        memset(buffer, 0, MAXLINE);
+        /* while (fgets(buffer, MAXLINE, file) != NULL) { */
+        /*   write(connection_fd, buffer, strlen(buffer)); */
+        /* } */
+        int bytes_read;
+        while ((bytes_read = fread(buffer, 1, MAXLINE, file)) > 0) {
+          write(connection_fd, buffer, bytes_read);
+        }
+
+        fclose(file);
+      }
+    } else {
+      write(connection_fd, "HTTP/1.1 501 Not Implemented\r\n\r\n", 31);
+    }
   }
-
-  char *method = strtok(buffer, " \t\r\n");
-  char *uri = strtok(NULL, " \t");
-  char *prot = strtok(NULL, " \t\r\n");
-
-  fprintf(stderr, "\x1b[32m + [%s] %s\x1b[0m\n", method, uri);
-
-  char *response = "HTTP/1.1 200 OK\r\n"
-                   "Content-Type: text/html\r\n"
-                   "Content-Length: 12\r\n"
-                   "\r\n"
-                   "Hello World!";
-
-  write(connection_fd, response, strlen(response));
 
   return NULL;
 }
